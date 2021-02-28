75f4cbf4bf9f4f3afae524105edfda138577b503
picassonk21 <picassonk21@gmail.com>
2021-02-28 23:31:51 +0300

    feat: add carbon-dating task solutin

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
+const validation = (activity) => {
+  return activity.split('').every(val => /[0-9]/.test(val))
+}
+const isInRange = (activity) => {
+  if(+activity <= 0 || +activity > 15) return false
+  return true
+}
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(activity) { 
+  if(!activity || (typeof activity) !== 'string' || !validation(activity) || !isInRange(activity)) return false
+  
+  return Math.ceil(Math.log(activity/MODERN_ACTIVITY) / K)
 };
