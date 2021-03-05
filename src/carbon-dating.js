1570a0aa9f2c7caaf8cac5f0fdddc4b57ff4f223
picassonk21 <picassonk21@gmail.com>
2021-03-05 11:16:49 +0300

    fix: change validation in carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,17 +2,15 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-const K = 0.693 / HALF_LIFE_PERIOD;
-const validation = (activity) => {
-  return activity.split('').every(val => /[0-9]/.test(val))
+const K = 0.693 / 5730
+const validate = (activity) => {
+  return typeof +activity === 'number'
 }
 const isInRange = (activity) => {
-  if(+activity <= 0 || +activity > 15) return false
-  return true
+  return +activity > 0 && +activity <= MODERN_ACTIVITY
 }
 
-module.exports = function dateSample(activity) { 
-  if(!activity || (typeof activity) !== 'string' || !validation(activity) || !isInRange(activity)) return false
-  
-  return Math.ceil(Math.log(activity/MODERN_ACTIVITY) / K)
+module.exports = function dateSample(activity) {
+  if(!activity || (typeof activity !== 'string') || !validate(activity) || !isInRange(activity)) return false 
+  return Math.ceil(Math.log(MODERN_ACTIVITY/+activity) / K)
 };
